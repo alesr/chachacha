38da28b869d3a8b458aa0912f9dacb51a131e442
Alessandro Resta <alessandro.resta@proton.me>
2025-03-17 11:54:05 +0200

    Define event messages for creating hosts and finding matches for players

---
+++ pkg/game/game.go
@@ -0,0 +1,20 @@
+package game
+
+// GameMode represents the mode of the game.
+// Eg.: 1v1, 2v2, classic, FreeForAll ...
+// It's up to the user of the engine to specify the game mode identifier.
+type GameMode string
+
+// HostRegistratioMessage represents the message payload for hosting a new game.
+type HostRegistratioMessage struct {
+	HostIP         string   `json:"host_ip"`
+	Mode           GameMode `json:"mode"`
+	AvailableSlots int8     `json:"available_slots"`
+}
+
+// MatchRequestMessage represents the message payload for joining an existing game.
+type MatchRequestMessage struct {
+	PlayerID string    `json:"player_id"`
+	HostIP   *string   `json:"host_ip,omitempty"`
+	Mode     *GameMode `json:"mode,omitempty"`
+}
